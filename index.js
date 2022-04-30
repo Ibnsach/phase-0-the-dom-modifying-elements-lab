2a76adabaff2d7760b5bc0b1a5a13e5d3df0309d
Adam Abass <adam.imoro@student.moringaschool.com>
2022-04-30 13:49:47 +0000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your code here!
+const element = document.createElement("div");
+
+document.body.append(element);
+
+const ul = document.createElement("ul");
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement("li");
+  li.textContent = (i + 1).toString();
+  ul.append(li);
+}
+
+element.append(ul);
+
+li.textContent = "Hi there!";
+
+console.log(li.textContent);
+
+const element = document.getElementById("main");
+element.style.height = "300px";
+element.style.backgroundColor = "#27647B";
+
+element.textContent = "You've changed what's on the screen!";
+
+element.style.fontSize = "24px";
+element.style.marginLeft = "30px";
+element.style.lineHeight = 2;
+
+element.className = "pet-listing dog";
